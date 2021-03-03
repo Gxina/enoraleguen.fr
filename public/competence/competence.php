b02e9295a8fc3ed35acc8941022935a89e53bfa4
Enora <enora1999@gmail.com>
2021-03-03 21:41:38 +0100

    Premier commit

---
+++ public/competence/competence.php
@@ -0,0 +1,128 @@
+
+<!DOCTYPE html>
+<!--/**
+    * \file     competence
+    * \version  1.1
+    * \date     Dernière modif 2020
+    * \author   Enora LE GUEN
+    * \details 	BDD: None
+*/ -->
+<html lang="fr">
+<?php $currentPage = 'Compétences'; ?>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel='icon' href='/EnoraPortefolio/logo.ico' type='image/x-icon' sizes="48x48" />
+    <script data-search-pseudo-elements defer src="https://pro.fontawesome.com/releases/v5.10.1/js/all.js"></script>
+    <link href="/EnoraPortefolio/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/EnoraPortefolio/css/mdb.min.css" rel="stylesheet">
+    <link href="/EnoraPortefolio/css/style-comp.css" rel="stylesheet">
+</head>
+<body>
+  <header>
+    <?php include('../navbar.php'); ?>
+
+<div id="intro" style="height:30em;"class="view">
+  <div id="intro" class="view">
+      <div class="mask rgba-black-strong">
+          <div class="container-fluid d-flex align-items-center justify-content-center h-100">
+              <div class="row d-flex justify-content-center text-center">
+                  <div class="col-md-10">
+                      <h2 class="display-4 font-weight-bold white-text pt-5 mb-2">Compétences</h2>
+                      <hr class="hr-light">
+                  </div>
+
+              </div>
+
+          </div>
+
+      </div>
+
+  </div>
+
+</div>
+  </header>
+  <main class="mt-5">
+      <div class="container">
+        <section id="best-features">
+          <section id="best-features" class="text-center">
+            <h2 class="mb-5">A propos de<span style="color:#009688;"> MOI</span></h2>
+              <div class="row">
+                <div class="col-md-4 mb-1">
+                  <i style="color:#009688;font-size: 3.5rem;" class="fad fa-users"></i>
+                  <h4 class="my-4">Management</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima
+                            assumenda deleniti hic.</p>
+                </div>
+                <div class="col-md-4 mb-1">
+                  <i style="color:#009688;font-size: 3.5rem;"class="fas fa-network-wired"></i>
+                  <h4 class="my-4">Réseaux</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima
+                            assumenda deleniti hic.</p>
+                </div>
+                <div class="col-md-4 mb-1">
+                  <i style="color:#009688;font-size: 3.5rem;" class="fas fa-theater-masks"></i>
+                  <h4 class="my-4">Danse Classique</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima
+                            assumenda deleniti hic.</p>
+                </div>
+                <div class="col-md-4 mb-1">
+                  <i style="color:#009688;font-size: 3.5rem;" class="fad fa-users"></i>
+                  <h4 class="my-4">Management</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima
+                            assumenda deleniti hic.</p>
+                </div>
+                <div class="col-md-4 mb-1">
+                  <i style="color:#009688;font-size: 3.5rem;"class="fas fa-network-wired"></i>
+                  <h4 class="my-4">Réseaux</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima
+                            assumenda deleniti hic.</p>
+                </div>
+                <div class="col-md-4 mb-1">
+                  <i style="color:#009688;font-size: 3.5rem;" class="fas fa-theater-masks"></i>
+                  <h4 class="my-4">Danse Classique</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit maiores nam, aperiam minima
+                            assumenda deleniti hic.</p>
+                </div>
+              </div>
+            </section>
+          </section>
+          <hr class="my-5">
+          <section id="gallery">
+            <table>
+              <colgroup>
+               <col>
+               <col style="background-color: yellow">
+             </colgroup>
+             <tr>
+               <th>Data 1</th>
+               <th>Data 2</th>
+             </tr>
+             <tr>
+               <td>Calcutta</td>
+               <td>Orange</td>
+             </tr>
+             <tr>
+               <td>Robots</td>
+               <td>Jazz</td>
+             </tr>
+            </table>
+          </section>
+          <hr class="my-5">
+            <section id="contact">
+              <h2 class="mb-5 font-weight-bold text-center">Mon parcours</h2>
+              <div class="row">
+                <div class="col-lg-5 col-md-12">
+
+                </div>
+              </div>
+            </section>
+        </div>
+  </main>
+
+<?php include('../footer.php'); ?>
+
+</body>
+
+</html>
