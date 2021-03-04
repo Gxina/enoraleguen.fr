87e9e3a504a6d8985580365d6e060c163ff6c02f
Enora <33421117+Gxina@users.noreply.github.com>
2021-03-04 21:56:17 +0100

    Update mdb.js

--- js/mdb.js
+++ js/mdb.js
@@ -25,6 +25,21 @@
  *
  */
 
+  // Realisation
+  var portfolioIsotope = $('.portfolio-container').isotope({
+    itemSelector: '.portfolio-item',
+    layoutMode: 'fitRows'
+  });
+
+  $('#portfolio-flters li').on( 'click', function() {
+    $("#portfolio-flters li").removeClass('filter-active');
+    $(this).addClass('filter-active');
+
+    portfolioIsotope.isotope({ filter: $(this).data('filter') });
+  });
+
+
+
 
 /*
 
